f55a7ca40552ac688366dcd3fcf8bbb82eeeac36
Stasiek99 <103383789+Stasiek99@users.noreply.github.com>
2022-04-26 16:48:37 +0200

    added a simple game (#1)
    * adding a simple game

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,40 @@ console.log('hello, world');
 const testMessage: string = 'TypeScript works';
 
 console.log(testMessage);
+
+function chooseNumber(): void {
+    const drawTicket = Math.floor((Math.random() * 100) + 1);
+    let number;
+    let howMany = 0;
+
+    do {
+        number = parseInt(prompt("Podaj liczbe !"));
+        howMany = howMany + 1
+
+        if (number > drawTicket) {
+            console.log("To za duzo")
+        } else if (number < drawTicket) {
+            console.log("To za malo")
+        } else if (number == drawTicket) {
+            console.log("Wygraleś")
+            let dec = prompt("Czy chcesz zagrac jeszcze raz ? T/N");
+            if (dec == "T") {
+                howMany = 0;
+                continue;
+            } else {
+                break;
+            }
+        }
+
+        if (howMany == 20 && number != drawTicket) {
+            console.log("Przegrales")
+            let dec = prompt("Czy chcesz zagrac jeszcze raz ? T/N");
+            if (dec == "T") {
+                howMany = 0;
+            } else {
+                break;
+            }
+        }
+    } while(number != drawTicket)
+}
+chooseNumber()
