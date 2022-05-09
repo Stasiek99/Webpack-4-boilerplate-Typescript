8cda5216b329cefc53cf96e91feeb5d915770392
Stasiek99 <stasiekpilich@gmail.com>
2022-05-09 21:24:30 +0200

    Added clone card function

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,19 @@ import {gameButton} from "./event-side-effects";
 import {connectorButton} from "./event-side-effects";
 import {gradientColor} from "./event-side-effects";
 import {chooseNumber} from "./event-side-effects";
+import {changeCSSClass} from "./event-side-effects";
+import {backToNormal} from "./event-side-effects";
+import {cloneCardButton} from "./event-side-effects";
+import {checkCardNumber} from "./clone-card-function";
 
 const testMessage: string = 'TypeScript works';
-
-console.log('hello, world');
 console.log(testMessage);
 
-document.getElementById("simpleGame").onmouseover = function() {cursorOnButton()};
-
+gameButton.addEventListener('mouseover', cursorOnButton)
 gameButton.addEventListener('click', chooseNumber);
 gameButton.addEventListener('click', changeColor);
 connectorButton.addEventListener('click', getPersonalData);
 gradientColor.addEventListener('click', gradientColorChange);
+gameButton.addEventListener("mouseover", changeCSSClass);
+gameButton.addEventListener("mouseout", backToNormal);
+cloneCardButton.addEventListener("click", checkCardNumber);
