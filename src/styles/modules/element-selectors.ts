2e91d143ebdf1558acfb6a842efe666f7de7765b
Stasiek99 <stasiekpilich@gmail.com>
2022-05-13 11:32:01 +0200

    added new-form-function and element-selectors

---
+++ src/styles/modules/element-selectors.ts
@@ -0,0 +1,6 @@
+export const gameButtonSelector= document.getElementById("simpleGame") as HTMLButtonElement;
+export const connectorButtonSelector= document.getElementById("personalInfo") as HTMLButtonElement;
+export const gradientColorSelector = document.getElementById("changeClass") as HTMLButtonElement;
+export const cloneCardButtonSelector = document.getElementById("cloneCard") as HTMLButtonElement;
+export const submitFormSelector = document.getElementById("form");
+export const formButtonSelector = document.getElementById("showFormButton") as HTMLButtonElement;
